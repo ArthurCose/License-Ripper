3294b65b6b59e27e4b576ebb34b832230ff52b22
Arthur Cosentino <arthurcose@gmail.com>
2025-01-28 16:27:03 -0500

    Bump dependencies, version, and loosen pnpm test

--- test/package-managers.ts
+++ test/package-managers.ts
@@ -24,8 +24,8 @@ test.serial("no-lock-file", async (t) => {
     fs.copyFile("package.json", path.join(folder, "package.json"))
   );
 
-  await testPackageManager(t, folder, baseOptions);
-  await testPackageManager(t, folder, includeDevOptions);
+  await testPackageManager(t, folder, baseOptions, false);
+  await testPackageManager(t, folder, includeDevOptions, false);
 });
 
 test.serial("pnpm", async (t) => {
@@ -36,8 +36,8 @@ test.serial("pnpm", async (t) => {
     fs.rename("pnpm-lock.yaml", path.join(folder, "pnpm-lock.yaml"));
   });
 
-  await testPackageManager(t, folder, baseOptions);
-  await testPackageManager(t, folder, includeDevOptions);
+  await testPackageManager(t, folder, baseOptions, true);
+  await testPackageManager(t, folder, includeDevOptions, true);
 });
 
 async function install(
@@ -80,7 +80,8 @@ async function install(
 async function testPackageManager(
   t: ExecutionContext,
   otherRoot: string,
-  options: Options
+  options: Options,
+  loose: boolean
 ) {
   const npm = await ripAll("", options);
   const other = await ripAll(otherRoot, options);
@@ -93,13 +94,30 @@ async function testPackageManager(
     p.path = "";
   }
 
-  const npmResolvedByName = keyByNameAndVersion(npm.resolved);
-  const otherResolvedByName = keyByNameAndVersion(other.resolved);
+  const otherResolvedByName = listStringsByName(other.resolved);
 
   // loop to see the exact packages that don't match
-  for (const key in npmResolvedByName) {
-    t.assert(otherResolvedByName[key], `expecting "${key}"`);
-    t.deepEqual(otherResolvedByName[key], npmResolvedByName[key]);
+  if (!loose) {
+    // check name and version
+    const npmResolvedByNV = keyByNameAndVersion(npm.resolved);
+    const otherResolvedByNV = keyByNameAndVersion(other.resolved);
+
+    for (const key in npmResolvedByNV) {
+      const value = npmResolvedByNV[key];
+      const matchList = otherResolvedByName[value.name] ?? [];
+
+      t.assert(
+        otherResolvedByNV[key],
+        `expecting "${key}", found: [${matchList.join(", ")}]`
+      );
+    }
+  } else {
+    // check just names
+    const npmResolvedByName = listStringsByName(other.resolved);
+
+    for (const key in npmResolvedByName) {
+      t.assert(otherResolvedByName[key], `expecting "${key}"`);
+    }
   }
 
   t.deepEqual(other.resolved.length, npm.resolved.length);
@@ -110,6 +128,23 @@ function keyByNameAndVersion(input: { name: string; version: string }[]) {
 
   for (const o of input) {
     map[o.name + "@" + o.version] = o;
+  }
+
+  return map;
+}
+
+function listStringsByName(input: { name: string; version: string }[]) {
+  const map: { [key: string]: string[] } = {};
+
+  for (const o of input) {
+    let stored = map[o.name];
+    const string = o.name + "@" + o.version;
+
+    if (stored) {
+      stored.push(string);
+    } else {
+      map[o.name] = [string];
+    }
   }
 
   return map;
